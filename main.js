e156a84e0a99ddc912dea91e55c2089521bb1b6a
Ishav bhatt <ishavbhattkhaniyara.com>
2020-08-04 12:39:31 +0530

    add player class

--- main.js
+++ main.js
@@ -44,6 +44,7 @@ class State {
     return this.actors.find((a) => a.type == "player");
   }
 }
+
 class Vec {
   constructor(x, y) {
     this.x = x;
@@ -56,3 +57,20 @@ class Vec {
     return new Vec(this.x * factor, this.y * factor);
   }
 }
+
+class Player {
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+  }
+
+  get type() {
+    return "player";
+  }
+
+  static create(pos) {
+    return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+  }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
